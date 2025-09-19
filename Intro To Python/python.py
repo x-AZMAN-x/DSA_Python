e9871caefa65ad3743c4c2edac7321392836f659
x-AZMAN-x <fromrimu@gmail.com>
2025-09-19 12:19:14 +0600

    intro to python dsa

---
+++ Intro To Python/python.py
@@ -0,0 +1,22 @@
+"""
+Python History
+
+It Is A High-Level, Interpreted, And General-Purpose Programming Language. It Was Invented Back In 1991. It Was Created By Guido Van Rossum. It Is Known For Easy-To-Read Syntax. Python Became Popular Because Of Its Simplicity And Readability, Making It A Favorite For Beginners And Experts Alike.
+
+Algorithm
+
+Algorithm Is A Step-By-Step Process For Any Kind Of Problem(Tasks), Which Gives You Correct Output For Each And Every Case.
+
+Variable
+
+A Variable Is Like A Named Place/Storage/Container In Computer's Memory Where It Can Store One Or Many Values/Data.
+"""
+
+num1, num2 = 10, 17.9845
+print(f"The Sum Of num1({num1}) And num2({num2}) = {num1+num2}")
+print("The Sum Of num1", num1,"And num2", num2,"=", num1+num2)
+print("The Value Of num2 = {0:.2f}".format(num2))
+print(f"The Value Of num2 = {num2:.2f}")
+print()
+print(num1, num2)
+print(num1,num2,sep="sep=>")
