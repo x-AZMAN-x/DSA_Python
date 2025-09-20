7d6e2305ac479b03f158a74370ee2f379441f9de
x-AZMAN-x <fromrimu@gmail.com>
2025-09-20 17:04:52 +0600

    acp for dsa python

---
+++ ACP/python.py
@@ -0,0 +1,96 @@
+#NO. 1
+a = 54
+b = 31
+
+# Addition
+print ('Sum: ', a + b)  
+
+# Subtraction
+print ('Subtraction: ', a - b)   
+
+# Multiplication
+print ('Multiplication: ', a * b)  
+
+# Division
+print ('Division: ', a / b) 
+
+# Floor Division
+print ('Floor Division: ', a // b)
+
+# Modulo
+print ('Modulo: ', a % b)  
+
+# a To The Power b
+print ('Power: ', a ** b)
+
+#NO.2
+# assign 10 to a
+a = 10
+
+# assign 5 to b
+b = 5 
+
+# assign the sum of a and b to a
+a += b
+
+print(a)
+
+#NO.3
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
+
+#NO. 4
+# logical AND
+print(True and True)
+print(True and False)
+
+# logical OR
+print(True or False)
+
+# logical NOT
+print(not True) 
+
+#NO. 5
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+print(x1 is not y1)
+
+print(x2 is y2)
+
+print(x3 is y3)
+
+#NO. 6
+message = 'Hello world'
+dict1 = {1:'a', 2:'b'}
+
+# check if 'H' is present in message string
+print('H' in message)
+
+# check if 'hello' is present in message string
+print('hello' not in message)
+
+# check if '1' key is present in dict1
+print(1 in dict1)
+
+# check if 'a' key is present in dict1
+print('a' in dict1)
