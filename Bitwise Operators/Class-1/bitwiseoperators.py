adf2f86982ba692830e6a0ddbe095598bda9ab97
x-AZMAN-x <fromrimu@gmail.com>
2025-11-28 12:15:04 +0600

    bitwise operations

---
+++ Bitwise Operators/Class-1/bitwiseoperators.py
@@ -0,0 +1,9 @@
+x = 10
+y = 5
+
+print(f"AND Operator For, {x} & {y} = {x&y}")
+print(f"OR Operator For, {x} | {y} = {x|y}")
+print(f"XOR Operator For, {x} ^ {y} = {x^y}")
+print(f"NOT Operator For, ~ {y} = {~y}")
+print(f"LEFT SHIFT Operator For, {x} << {y} = {x<<y}")
+print(f"RIGHT SHIFT Operator For, {x} >> {y} = {x>>y}")
