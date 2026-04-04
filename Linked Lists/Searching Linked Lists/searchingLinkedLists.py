34d9ad4128a564283a58b5703931fb1b5a6d0182
x-AZMAN-x <fromrimu@gmail.com>
2026-04-04 13:03:57 +0600

    searching linked list

---
+++ Linked Lists/Searching Linked Lists/searchingLinkedLists.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data):
+        self.data = None
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last = self.head
+        while last.next:
+            last = last.next
+
+        last.next = new_node
+        new_node.prev = last
+        
+    def display(self):
+        if self.head is None:
+            print("List Is Empty")
+            return
+        current = self.head
+        print("None <->", end = " ")
+        while current:
+            print(current.data, end = " <-> ")
+            current = current.next
+            print("None")
+
+    def search(self, key):
+        current = self.head
+        position = 0
+        while current:
+            if current.data == key:
+                return current, position
+            current = current.next
+            position += 1
+            return None, -1
+        
+if __name__ == "__main__":
+    dll = DoublyLinkedList()
+    dll.append(10) #These Values Memory Address Will Be Discrete Originally
+    dll.append(20)
+    dll.append(30)
+    dll.append(40)
+    dll.append(50)
+    for value in [60, 70, 80, 90, 100]: #These Values Memory Address Will Be Ordered As They Belong To A Normal List Orignially
+        dll.append(value)
+    print("Original Doubly Linked List:")
+    dll.display()
+
+    print()
+    print("--- SEARCHING ---")
+    node, pos = dll.search(40)
+    if node:
+        print(f"40 found at position {pos}.")
+    else:
+        print("40 not found.")
