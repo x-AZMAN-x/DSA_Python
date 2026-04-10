3b174d1071850dc73d58b6e5d71f347191e8dd03
x-AZMAN-x <fromrimu@gmail.com>
2026-04-10 15:02:34 +0600

    matrix acp

---
+++ Matrix ACP/matrixACP.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+A = np.array([1, 2], [3, 4])
+B = np.array([5, 6], [7, 8])
+
+print("Addition: \n", A + B)
+print("Addition: \n", A - B)
+print("Addition: \n", A @ B) #@ Is The Mtrix Multiply Operator
