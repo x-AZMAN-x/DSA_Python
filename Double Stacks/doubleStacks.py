13455b1ef08d4664e4a7215a8b6753bf181653cc
x-AZMAN-x <fromrimu@gmail.com>
2026-05-15 12:05:00 +0600

    double stacks

---
+++ Double Stacks/doubleStacks.py
@@ -0,0 +1,26 @@
+class DoubleStack:
+    def __init__(self, capacity):   # Capacity Is The Size Of The Combined Array
+        self.capacity = capacity   # Total Size Of The Array
+        self.arr = [None] * capacity   # One Array Holding Both Stacks
+        self.top1 = -1   # Stack1 Starts Empty From Left
+        self.top2 = capacity   # Stack2 Starts Empty From Right
+
+    # Stack1
+    def push1(self, item):
+        if self.top1 + 1== self.top2:
+            raise OverflowError("Double Stack Is Full, You Can't Push Anymore.")
+        self.top1 += 1
+        self.arr[self.top1] = item
+
+    def pop1(self):
+        if self.top1 == -1:
+            raise IndexError("Stack Is Empty.")
+        item = self.arr[self.top1]
+        self.arr[self.top1] = None   # Optional: Clear Reference
+        self.top1 -= 1
+        return item
+    
+    def peek1(self):
+        if self.top1 == -1:
+            return None
+        return self.arr[self.top1]
